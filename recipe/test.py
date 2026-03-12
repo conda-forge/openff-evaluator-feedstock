d5e9e1a528d50d2fb3249855abab58d37d87fa58
Matthew W. Thompson <mattwthompson@protonmail.com>
2026-03-12 09:11:58 -0500

    Update test script

--- recipe/test.py
+++ recipe/test.py
@@ -1,3 +1,6 @@
 from openff.evaluator.utils.packmol import _find_packmol
+from openff.evaluator import __version__
+
+assert __version__ != "0.0.0", f"Version mangled! Found {__version__=}"
 
 assert _find_packmol() is not None
