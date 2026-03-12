320fb0cd8330cc03e95a2551b3c5982841f57544
Matthew W. Thompson <mattwthompson@protonmail.com>
2026-03-12 09:11:03 -0500

    Release 0.5.3

--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -1,1 +1,1 @@
-${PYTHON} -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
+SETUPTOOLS_SCM_PRETEND_VERSION="$PKG_VERSION" ${PYTHON} -m pip install . --no-deps --ignore-installed --no-cache-dir -vv
